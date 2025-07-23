fb922df672c46f1e8361a8726b08e6c7841d7320
Viren Soni <virensoni2901@gmail.com>
2025-07-23 13:15:46 +0530

    Did some JS

--- Demo/script.js
+++ Demo/script.js
@@ -1,6 +1,67 @@
-// const birthYear = 2007
+// let birthYear = 2007
 
 // console.log(birthYear)
 
 // birthYear = 2005
 
+// Camel Case : isJsFun
+
+// isActive = true && false;
+// console.log(isActive)
+
+// Falsy => undefined, null, 0, false, '', -0, NaN
+// Truthy => !0 and !''
+
+// let email = '';
+
+// if (email) {
+//     console.log('Email sent');
+// }
+
+// 7>7 ? console.log(true) : console.log(false)
+
+// if () {
+
+// }
+// else if () {
+
+// }
+// else {
+
+// }
+
+
+// let test = (a, b) => {
+//     return [a,b]
+// }
+
+// console.log(test(4, 5))
+
+// // Callback function
+// function test2() {
+//     console.log('this fn is called by another function')
+// }
+
+// function test3(fn) {
+//     test2();
+// }
+
+// test3(test2) // This is callback function
+
+
+
+// let laptop = {
+//     brandName : 'ASUS',
+//     price : 65990
+// }
+
+// let cpu = 'Core i5-12500H'
+// laptop['cpu'] = cpu
+
+// console.log(laptop)
+
+
+// let arr = [1,2,3,4,5,6]
+
+// const [first, second, third, fourth] = arr
+// console.log(first,second,third,fourth)
