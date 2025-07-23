dfad61229c105e864d1323b506ecc54db8bdf9ec
Viren Soni <virensoni2901@gmail.com>
2025-07-23 14:03:13 +0530

    Before react

--- Demo/script.js
+++ Demo/script.js
@@ -38,13 +38,13 @@
 // console.log(test(4, 5))
 
 // // Callback function
-// function test2() {
-//     console.log('this fn is called by another function')
-// }
+function test2() {
+    console.log('this fn is called by another function')
+}
 
-// function test3(fn) {
-//     test2();
-// }
+function test3(fn) {
+    test2();
+}
 
 // test3(test2) // This is callback function
 
@@ -61,7 +61,50 @@
 // console.log(laptop)
 
 
-// let arr = [1,2,3,4,5,6]
+let arr = [1,2,3,4,5,6]
 
-// const [first, second, third, fourth] = arr
-// console.log(first,second,third,fourth)
+const [first, second, third, fourth] = arr
+console.log(first,second,third,fourth)
+
+
+let even = [2,4,6,8]
+
+const [firstVal, , thirdVal] = even
+console.log(firstVal, thirdVal)
+
+
+let myDetails = {
+    name: 'TEST',
+    email: 'test@yourcompany.com'
+}
+
+const {name, email} = myDetails
+console.log(name, email)
+
+
+let odd = [1,3,5,7,9]
+const copyOdd = [...odd]
+console.log(copyOdd)
+
+let frontend = ['HTML', 'CSS', 'JS', 'React']
+let backend = ['Node', 'Express', 'MongoDB']
+
+const MERN = [...frontend, ...backend]
+console.log(MERN)
+
+// Same works in the case of object
+
+// Asynchronous
+// setTimeout(test3, 3000)
+
+
+const res = fetch('https://jsonplaceholder.typicode.com/todos').then(res =>  {
+    return res.json()
+}).then(data => {
+    console.log(data)
+})
+
+
+
+// promise = object
+// promise is a placeholder
